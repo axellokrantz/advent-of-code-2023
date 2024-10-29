ed940fa0e4b0baa3646f65aef0b6c143a36b42a4
axellokrantz <axel.lokrantz@gmail.com>
2024-10-29 20:00:06 +0100

    solution

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,14 @@
+// Sonny Li
+// Fun Fact: I played guitar in a band called Attica.
+
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!\n";
+    std::cout << "  A\n";
+    std::cout << " A A\n";
+    std::cout << "A   A\n";
+    std::cout << "AAAAA\n";
+    std::cout << "A   A\n";
+    std::cout << "A   A\n";
+    std::cout << "A   A\n";
 }
