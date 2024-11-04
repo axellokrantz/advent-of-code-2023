debb79ca7b83f5693960875ff32343caac7d9ed6
axellokrantz <axel.lokrantz@gmail.com>
2024-11-04 21:10:14 +0100

    done

--- simple_looping/8.cpp
+++ simple_looping/8.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 std::string reversed_triple_chars(const std::string& s) {
     
-    return "";
+    std::string result = "";
+    for (size_t i = 0; i < s.size(); i+= 3)
+    {
+        if(s.size() - i < 3){
+            result += s.substr(i);
+            break;
+        }
+        std::string sub_string = s.substr(i, 3);
+        std::reverse(sub_string.begin(), sub_string.end());
+        result += sub_string;
+    }
+    
+    return result;
 }
 
 int main(){
-    std::cout << reversed_triple_chars("abcdef");
+    std::cout << reversed_triple_chars("abcdefgh");
 }
