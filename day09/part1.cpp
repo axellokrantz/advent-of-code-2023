20b7139808809a5e581f897ddc43987882d0f7b9
axellokrantz <axel.lokrantz@gmail.com>
2024-11-19 16:56:07 +0100

    day09 part1

--- day09/part1.cpp
+++ day09/part1.cpp
@@ -5,9 +5,14 @@
 #include <string>
 #include <vector>
 
+bool allZeroes(const std::vector<int>& sequence) {
+    return std::all_of(sequence.begin(), sequence.end(), [](int x) { return x == 0; });
+}
+
 int main() {
     std::ifstream input_text("input.txt");
     std::string current_line;
+    long long total_sum = 0;
 
     while (std::getline(input_text, current_line)) {
         std::vector<std::vector<int>> sequences;
@@ -19,9 +24,9 @@ int main() {
         }
 
         sequences.push_back(sequence);
-        bool allZeroes = false;
+        bool all_zero_flag = false;
 
-        while (!allZeroes) {
+        while (!all_zero_flag) {
             std::vector<int> temp;
             for (int i = 0; i < sequence.size() - 1; i++) {
                 temp.push_back(sequence[i + 1] - sequence[i]);
@@ -29,10 +34,17 @@ int main() {
             sequence = temp;
             sequences.push_back(sequence);
 
-            allZeroes = std::all_of(sequence.begin(), sequence.end(),
-                                    [](int x) { return x == 0; });
+            all_zero_flag = allZeroes(sequence);
         }
 
-        
+        for (int i = sequences.size() - 1; i >= 1; i--) {
+            if (allZeroes(sequences[i])) {
+                sequences[i].push_back(0);
+            }
+            sequences[i - 1].push_back(sequences[i].back() +
+                                       sequences[i - 1].back());
+        }
+        total_sum += sequences[0].back();
     }
+    std::cout << total_sum << std::endl;
 }
